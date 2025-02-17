33baa17b8332f08c8ed821c015432668bd9c3599
0x1755 <>
2025-02-17 15:00:34 +0700

    update

--- agent-as-a-service/agent-orchestration/backend/serializers/agent_serializers.go
+++ agent-as-a-service/agent-orchestration/backend/serializers/agent_serializers.go
@@ -338,7 +338,10 @@ func NewAgentInfoResp(m *models.AgentInfo) *AgentInfoResp {
 	} else {
 		if m.TokenInfo != nil && m.TokenAddress != "" && m.TokenInfo.PriceUsd.Cmp(big.NewFloat(0)) > 0 {
 			resp.Meme = NewMemeFromTokenInfoResp(m.TokenInfo, m)
-			if m.Meme != nil {
+			if m.TokenMode == string(models.CreateTokenModeTypeLinkExisting) {
+				resp.Meme.Status = string(models.MemeStatusAddPoolExternal)
+				// resp.Meme.TradeUrl =
+			} else if m.Meme != nil {
 				resp.Meme.Supply = m.Meme.Supply
 				if m.Meme.Status == models.MemeStatusAddPoolExternal {
 					resp.Meme.Status = string(models.MemeStatusAddPoolExternal)
