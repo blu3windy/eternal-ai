9dcd7c7a26d1a8bc1e9e0f782d223ac9a96064df
eai1111 <eai1111@gmail.com>
2025-02-17 14:43:30 +0000

    disable game tasks (again)

--- ai-architectures/agent-task-handlers/x_content/service.py
+++ ai-architectures/agent-task-handlers/x_content/service.py
@@ -41,8 +41,8 @@ def task_cls_resolver(log: ReasoningLog) -> Optional[MultiStepTaskBase]:
     if log.task == AgentTask.POST_V2:
         return tasks.social_agent.post_v2.PostV2
 
-    # if log.task == AgentTask.CREATE_GAME or log.task == AgentTask.JUDGE_GAME:
-    #     return tasks.others.FallbackTask
+    if log.task == AgentTask.CREATE_GAME or log.task == AgentTask.JUDGE_GAME:
+        return tasks.others.FallbackTask
 
     if log.task == AgentTask.CREATE_GAME:
         return tasks.game_agent.create_gamev2.GameReplyTask
