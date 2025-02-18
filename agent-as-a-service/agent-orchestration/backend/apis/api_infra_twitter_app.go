415b5a65049c9937e7821a8ed886b37c5466d071
0xaf16 <>
2025-02-18 15:13:37 +0700

    update

--- agent-as-a-service/agent-orchestration/backend/apis/api_infra_twitter_app.go
+++ agent-as-a-service/agent-orchestration/backend/apis/api_infra_twitter_app.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/eternalai-org/eternal-ai/agent-as-a-service/agent-orchestration/backend/errs"
-	"github.com/eternalai-org/eternal-ai/agent-as-a-service/agent-orchestration/backend/helpers"
 	"github.com/eternalai-org/eternal-ai/agent-as-a-service/agent-orchestration/backend/serializers"
 	"github.com/gin-gonic/gin"
 )
@@ -13,8 +12,9 @@ func (s *Server) InfraTwitterAppAuthenInstall(c *gin.Context) {
 	ctx := s.requestContext(c)
 	authUrl, err := s.nls.InfraTwitterAppAuthenInstall(
 		ctx,
+		s.stringFromContextQuery(c, "address"),
 		s.stringFromContextQuery(c, "install_uri"),
-		s.stringFromContextQuery(c, "install_code"),
+		s.stringFromContextQuery(c, "signature"),
 	)
 	if err != nil {
 		ctxAbortWithStatusJSON(c, http.StatusBadRequest, &serializers.Resp{Error: errs.NewError(err)})
@@ -33,17 +33,17 @@ func (s *Server) InfraTwitterAppAuthenCallback(c *gin.Context) {
 	c.Redirect(http.StatusFound, returnUri)
 }
 
-func (s *Server) InfraTwitterAppExecuteRequest(c *gin.Context) {
-	ctx := s.requestContext(c)
-	var req interface{}
-	if err := c.ShouldBindJSON(&req); err != nil {
-		ctxJSON(c, http.StatusBadRequest, &serializers.Resp{Error: errs.NewError(err)})
-		return
-	}
-	res, err := s.nls.InfraTwitterAppExecuteRequest(ctx, c.GetHeader("infra-code"), c.GetHeader("api-key"), helpers.ConvertJsonString(req))
-	if err != nil {
-		ctxAbortWithStatusJSON(c, http.StatusBadRequest, &serializers.Resp{Error: errs.NewError(err)})
-		return
-	}
-	ctxSTRING(c, http.StatusOK, res)
-}
+// func (s *Server) InfraTwitterAppExecuteRequest(c *gin.Context) {
+// 	ctx := s.requestContext(c)
+// 	var req interface{}
+// 	if err := c.ShouldBindJSON(&req); err != nil {
+// 		ctxJSON(c, http.StatusBadRequest, &serializers.Resp{Error: errs.NewError(err)})
+// 		return
+// 	}
+// 	res, err := s.nls.InfraTwitterAppExecuteRequest(ctx, c.GetHeader("infra-code"), c.GetHeader("api-key"), helpers.ConvertJsonString(req))
+// 	if err != nil {
+// 		ctxAbortWithStatusJSON(c, http.StatusBadRequest, &serializers.Resp{Error: errs.NewError(err)})
+// 		return
+// 	}
+// 	ctxSTRING(c, http.StatusOK, res)
+// }
