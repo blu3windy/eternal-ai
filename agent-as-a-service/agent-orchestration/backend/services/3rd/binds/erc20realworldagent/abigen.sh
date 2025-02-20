6a6c325118aeb4c283005f3cc25b677ebb0283fd
0xaf16 <>
2025-02-20 17:20:36 +0700

    update

--- agent-as-a-service/agent-orchestration/backend/services/3rd/binds/erc20realworldagent/abigen.sh
+++ agent-as-a-service/agent-orchestration/backend/services/3rd/binds/erc20realworldagent/abigen.sh
@@ -1,1 +1,5 @@
-abigen --bin=ERC20RealWorldAgent.bin --abi=ERC20RealWorldAgent.abi --pkg=erc20realworldagent --type=ERC20RealWorldAgent --out=ERC20RealWorldAgent.go
+jq ".abi" ERC20RealWorldAgent.json > ERC20RealWorldAgent.abi
+jq -r ".bytecode.object" ERC20RealWorldAgent.json > ERC20RealWorldAgent.bin
+abigen --bin=ERC20RealWorldAgent.bin --abi=ERC20RealWorldAgent.abi --pkg=erc20realworldagent --type ERC20RealWorldAgent --out=ERC20RealWorldAgent.go
+rm ERC20RealWorldAgent.abi
+rm ERC20RealWorldAgent.bin
--- agent-as-a-service/agent-orchestration/backend/services/3rd/binds/erc20utilityagent/abigen.sh
+++ agent-as-a-service/agent-orchestration/backend/services/3rd/binds/erc20utilityagent/abigen.sh
@@ -1,3 +1,5 @@
 jq ".abi" ERC20UtilityAgent.json > ERC20UtilityAgent.abi
 jq -r ".bytecode" ERC20UtilityAgent.json > ERC20UtilityAgent.bin
 abigen --bin=ERC20UtilityAgent.bin --abi=ERC20UtilityAgent.abi --pkg=erc20utilityagent --type ERC20UtilityAgent --out=ERC20UtilityAgent.go
+rm ERC20UtilityAgent.abi
+rm ERC20UtilityAgent.bin
