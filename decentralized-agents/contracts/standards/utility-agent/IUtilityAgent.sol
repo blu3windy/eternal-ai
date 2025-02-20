7f03f1d6e45f7747f269401b6a5d55e7e5012e28
eai-dev <dev@eternalai.org>
2025-02-20 17:09:57 +0700

    Add LLM and Utility Agent interfaces and contracts

--- decentralized-agents/contracts/standards/utility-agent/IUtilityAgent.sol
+++ decentralized-agents/contracts/standards/utility-agent/IUtilityAgent.sol
@@ -2,37 +2,30 @@
 pragma solidity ^0.8.0;
 
 import {File} from "./IFileStore.sol";
+import {ICommonAgent} from "./ICommonAgent.sol";
 
-interface IUtilityAgent {
+interface IUtilityAgent is ICommonAgent {
     struct StorageInfo {
         address contractAddress;
         string filename;
     }
 
-    function getPromptSchedulerAddress() external view returns (address);
-    function getModelAddress() external view returns (address);
-    function getSystemPrompt() external view returns (string memory);
-
-    function prompt(bytes calldata request) external returns (uint256);
-    function getResultById(uint256 id) external view returns (bytes memory);
+    event ForwardPerformed(
+        bytes32 indexed uuid,
+        uint256 indexed inferId,
+        address indexed caller,
+        bytes fowardData
+    );
 
     function fetchCode() external view returns (string memory logic);
     function getStorageMode() external view returns (string memory);
     function getStorageInfo() external view returns (StorageInfo memory);
     function getFileStorageChunkInfo() external view returns (File memory file);
 
-    function updateSystemPrompt(string memory _systemPrompt) external;
     function updateFileName(string memory filename) external;
-
-    event PromptSchedulerHubUpdate(address promptScheduler);
-    event ModelUpdate(address hybridModel);
-    event SystemPromptUpdate(string systemPrompt);
-    event PromptPerformed(
-        address indexed caller,
-        uint256 indexed inferId,
-        bytes request
-    );
-
-    error InvalidAddress();
-    error InvalidData();
+    function forward(
+        bytes32 uuid,
+        address dstAgent,
+        bytes memory request
+    ) external payable returns (uint256);
 }
