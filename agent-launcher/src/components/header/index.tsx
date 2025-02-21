6c72526f8ebd7f73e996da0ceb8197c9ac70d00e
0xmegalodon <193847696+0x11235@users.noreply.github.com>
2025-02-21 14:13:29 +0700

    -: update header logo

--- agent-launcher/src/components/header/index.tsx
+++ agent-launcher/src/components/header/index.tsx
@@ -1,14 +1,15 @@
-import { Flex, Text } from "@chakra-ui/react";
+import {Flex, Link} from "@chakra-ui/react";
 import HeaderMenu from "./menu";
 import HeaderWallet from "./wallet";
 import s from "./styles.module.scss";
+import ROUTERS from "../../constants/route-path.ts";
 
 type Props = {};
 
 const Header = (_props: Props) => {
   return (
     <Flex className={s.headerContainer}>
-      <Text>EternalAI</Text>
+      <Link href={ROUTERS.HOME} fontSize={"18px"} fontWeight={500} textDecoration={"unset !important"}>Eternal AI</Link>
       <HeaderMenu />
       <HeaderWallet />
     </Flex>
