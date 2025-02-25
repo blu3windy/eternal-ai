ddcb955328d00350d31d07b5217b003c401063d5
0x5123 <ngxx8651@gmail.com>
2025-02-25 11:03:21 +0700

    Update layout

--- agent-launcher/src/components/header/index.tsx
+++ agent-launcher/src/components/header/index.tsx
@@ -1,4 +1,4 @@
-import {Flex, Link} from "@chakra-ui/react";
+import { Flex, Link } from "@chakra-ui/react";
 import HeaderMenu from "./menu";
 import HeaderWallet from "./wallet";
 import s from "./styles.module.scss";
@@ -7,13 +7,14 @@ import ROUTERS from "../../constants/route-path.ts";
 type Props = {};
 
 const Header = (_props: Props) => {
-  const primaryColor = 'black';
+  const primaryColor = "black";
 
   return (
     <Flex className={s.headerContainer}>
-      <Link href={ROUTERS.HOME} fontSize={"18px"} fontWeight={500} textDecoration={"unset !important"}>Eternal AI</Link>
+      <HeaderMenu />
+      {/* <Link href={ROUTERS.HOME} fontSize={"18px"} fontWeight={500} textDecoration={"unset !important"}>Eternal AI</Link>
       <HeaderMenu primaryColor={primaryColor}/>
-      <HeaderWallet color={primaryColor}/>
+      <HeaderWallet color={primaryColor}/> */}
     </Flex>
   );
 };
