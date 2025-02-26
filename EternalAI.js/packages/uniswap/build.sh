502b3243728f5b50f1653186f20466a4b755fb05
0xgen0000 <52655944+0xgen0000@users.noreply.github.com>
2025-02-26 14:17:34 +0700

    Revert "- build"
    This reverts commit ce7f47c79c5968fa91d4285edc5e1eb1ccbe3fa1.

--- EternalAI.js/packages/uniswap/build.sh
+++ EternalAI.js/packages/uniswap/build.sh
@@ -9,4 +9,3 @@ base64 -i ./dist/bundle.iife.js.gz -o ./dist/bundle.iife.js.gz.base64
 sed -i '' 's|^|data:@file/gzip;base64,|' ./dist/bundle.iife.js.gz.base64
 ls -lg -h dist/
 
-sed "s|BUILD_FILE|$(cat ./dist/bundle.iife.js.gz.base64)|g" ./ui/index_template.html > ./ui/index.html
