a5a93ee6cfc3eb6ab1ea557cef8f52f681748bc8
0x999999999 <agent.whiskey.dev@gmail.com>
2025-02-26 14:14:09 +0700

    feat: Add new node creation card to node list
    - Implemented a new card component for creating nodes
    - Used CardBase and SVG components for consistent design
    - Added icon and text for "run a new node" functionality

--- agent-launcher/src/pages/mine/features/node-list/index.tsx
+++ agent-launcher/src/pages/mine/features/node-list/index.tsx
@@ -1,6 +1,9 @@
 import { Flex } from '@chakra-ui/react'
 import NodeCard from '../../components/node-card'
 import { useNodes } from '../../stores/useNodes'
+import CardBase from '../../components/card-base';
+import SVG from 'react-inlinesvg';
+import styles from './styles.module.scss';
 
 const NodeList = () => {
   const nodes = useNodes(state => state.items);
@@ -10,6 +13,14 @@ const NodeList = () => {
       {nodes.map((node) => (
         <NodeCard key={node.id} {...node} />
       ))}
+
+      <CardBase className={styles.newCard} borderColor='#000000'>
+        <Flex width={'100%'} height={'100%'} alignItems={'center'} justifyContent={'center'} flexDirection={'column'}>
+          <SVG src='/icons/ic_48_laptop-code.svg' width={48} height={48} className={styles.newCard_icon} />
+
+          <p className={styles.newCard_text}>run a new node</p>
+        </Flex>
+      </CardBase>
     </Flex>
   )
 }
