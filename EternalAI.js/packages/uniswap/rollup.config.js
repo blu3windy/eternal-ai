e3ffe42c7293c4e816caffd13e909f94dd0b08b3
0xgen0000 <52655944+0xgen0000@users.noreply.github.com>
2025-02-26 12:08:42 +0700

    - swap

--- EternalAI.js/packages/uniswap/rollup.config.js
+++ EternalAI.js/packages/uniswap/rollup.config.js
@@ -1,27 +1,28 @@
 const resolve = require('@rollup/plugin-node-resolve').default;
 const commonjs = require('@rollup/plugin-commonjs');
 const typescript = require('@rollup/plugin-typescript');
-const { terser } = require('rollup-plugin-terser');
+const {terser} = require('rollup-plugin-terser');
 const pkg = require('./package.json');
 import json from '@rollup/plugin-json';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 
 module.exports = {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/bundle.iife.js',
-      format: 'iife',
-      name: 'UniSwap',
-    }
-  ],
-  plugins: [
-    resolve(),
-    commonjs(),
-    typescript({ tsconfig: './build.tsconfig.json' }),
-    terser(),
-    json(),
-    nodePolyfills()
-  ],
-  external: Object.keys(pkg.peerDependencies || {}),
+    input: 'src/index.ts',
+    output: [
+        {
+            file: 'dist/bundle.iife.js',
+            format: 'iife',
+            name: 'UniSwap',
+            sourcemap: true
+        }
+    ],
+    plugins: [
+        resolve(),
+        commonjs(),
+        typescript({tsconfig: './build.tsconfig.json', sourceMap: true}),
+        terser(),
+        json(),
+        nodePolyfills()
+    ],
+    external: Object.keys(pkg.peerDependencies || {}),
 };
