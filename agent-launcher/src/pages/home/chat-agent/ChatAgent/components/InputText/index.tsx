3dc6729d46f960b401405c09e61eda961f4953ce
0x11235 <193847696+0x11235@users.noreply.github.com>
2025-02-26 15:38:43 +0700

    -: update UI chat

--- agent-launcher/src/pages/home/chat-agent/ChatAgent/components/InputText/index.tsx
+++ agent-launcher/src/pages/home/chat-agent/ChatAgent/components/InputText/index.tsx
@@ -41,24 +41,24 @@ const InputText = ({ onFocus, btnSubmit, isSending }: IProps) => {
       // position={'absolute'}
       // bottom={'10px'}
       paddingBottom={'10px'}
-      paddingLeft={'10px'}
-      paddingRight={'10px'}
-      background="#101216"
+      // paddingLeft={'10px'}
+      // paddingRight={'10px'}
+      // background="#101216"
     >
       <Flex
         w={{ base: '100%' }}
         padding="1px"
-        minHeight={'48px'}
+        // minHeight={'48px'}
         direction={'row'}
         gap={'12px'}
       >
         <Flex
-          border="1px solid rgba(255, 255, 255, 0.15)"
+          border="1px solid #5400FB33"
           borderRadius="10000px"
           flex={1}
-          backgroundColor="#000000"
-          boxShadow={'0px 0px 24px -6px rgba(84, 0, 251, 0.12)'}
-          minHeight={'48px'}
+          // backgroundColor="#000000"
+          boxShadow={'0px 0px 24px -6px #5400FB1F'}
+          minHeight={'60px'}
           overflow="hidden"
           position="relative"
         >
@@ -71,10 +71,11 @@ const InputText = ({ onFocus, btnSubmit, isSending }: IProps) => {
               width: '100%',
               height: '100%',
               outline: 'none',
-              padding: '13px 20px',
+              padding: '16px 20px',
               backgroundColor: 'transparent',
-              color: '#fff',
-              fontSize: '14px',
+              color: '#000',
+              fontSize: '16px',
+              fontWeight: '400',
               transform: 'height 0.2s ease',
             }}
             // disabled={loading}
@@ -105,12 +106,12 @@ const InputText = ({ onFocus, btnSubmit, isSending }: IProps) => {
             top="0"
             bottom="0"
             right="0"
-            paddingRight="10px"
+            // paddingRight="10px"
             cursor="pointer"
             justifyContent="center"
             alignItems="center"
-            width="34px"
-            backgroundColor="#000000"
+            width="50px"
+            backgroundColor="#FFF"
             opacity={isSending || isStopReceiving ? 0.2 : 1}
             onClick={() => {
               if (isSending || isStopReceiving) {
@@ -130,7 +131,7 @@ const InputText = ({ onFocus, btnSubmit, isSending }: IProps) => {
             >
               <path
                 d="M15.3724 2.55248L4.44486 5.28416C1.83403 5.93666 1.85986 9.65666 4.47986 10.2733L7.87911 11.0733C7.96328 11.0933 8.05245 11.0683 8.11328 11.0067L12.0616 7.05832C12.3033 6.81665 12.7032 6.81665 12.9448 7.05832C13.1865 7.29999 13.1865 7.69999 12.9448 7.94165L8.99483 11.8916C8.934 11.9525 8.9083 12.0416 8.9283 12.1258L9.72746 15.5217C10.3441 18.1417 14.064 18.1675 14.7165 15.5567L17.4482 4.62915C17.7607 3.37415 16.6249 2.23914 15.3724 2.55248Z"
-                fill="#fff"
+                fill="#000"
               />
             </svg>
           </Flex>
