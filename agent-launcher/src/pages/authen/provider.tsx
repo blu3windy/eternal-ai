fa4ffcd18013d9ad7dcb3dd14959f716ae355367
0x0603 <villatoto063@gmail.com>
2025-02-26 17:41:07 +0700

    _: fix conflict

--- agent-launcher/src/pages/authen/provider.tsx
+++ agent-launcher/src/pages/authen/provider.tsx
@@ -1,50 +1,78 @@
 import { createContext, useContext, useState, FC, PropsWithChildren, useMemo, useEffect } from 'react';
 import HomeAuthen from "./Home";
 import { Wallet } from "ethers";
+import EaiSigner from "../../helpers/signer";
+import sleep from "@utils/sleep.ts";
 
 interface AuthContextType {
    signer: Wallet | undefined;
+   hasUser: boolean;
+   onLogin: (pass: string) => void;
 }
-
-const TESTING_AUTHEN_FLAG = false;
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 const AuthProvider: FC<PropsWithChildren> = ({ children }) => {
-   const [loading, setLoading] = useState(true);
+   const [initLoading, setInitLoading] = useState<boolean>(true);
    const [signer, setSigner] = useState<Wallet | undefined>();
+   const [hasUser, setHasUser] = useState<boolean>(false);
+
+   const onLogin = async (pass: string) => {
+      try {
+         const prvKey = await EaiSigner.getStorageKey({ pass });
+         if (prvKey) {
+            const _signer = new Wallet(prvKey);
+            setSigner(_signer);
+            setHasUser(true);
+         }
+      } catch (error) {
+         console.error(error);
+      }
+   }
+
+   const init = async () => {
+      try {
+         setInitLoading(true);
+         const hasUser = await EaiSigner.hasUser();
+         setHasUser(hasUser);
+      } catch (error) {
+         console.error(error);
+      } finally {
+         await sleep(1000)
+         setInitLoading(false);
+      }
+   }
 
    useEffect(() => {
-      setInterval(() => {
-         setLoading(false)
-      }, 0)
+      init().then().catch();
    }, []);
 
    const values = useMemo(() => {
       return {
          signer,
+         hasUser,
+         onLogin
       }
-   }, [signer]);
+   }, [signer, hasUser]);
 
-   if (TESTING_AUTHEN_FLAG) {
-      return (
-         <AuthContext.Provider value={values}>
-            {loading
-               ? <div>Loading...</div>
-               : signer
-                  ? (children)
-                  : (<HomeAuthen />)
-            }
-         </AuthContext.Provider>
-      );
-   } else {
-      return (
-         <AuthContext.Provider value={values}>
-            {/* <HomeAuthen /> */}
-            {children}
-         </AuthContext.Provider>
-      );
+
+   const renderContent = () => {
+      if (initLoading) {
+         return <div>Loading...</div>;
+      } else {
+         if (signer) {
+            return children;
+         } else {
+            return <HomeAuthen />;
+         }
+      }
    }
+
+   return (
+      <AuthContext.Provider value={values}>
+         {renderContent()}
+      </AuthContext.Provider>
+   );
 };
 
 const useAuth = () => {
