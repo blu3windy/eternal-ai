0a828ff4525f485adfe8e421e86ba459e4e7d835
0x8506 <otis08506@gmail.com>
2025-02-27 16:47:45 +0700

    feat: read file onchain

--- agent-launcher/electron/ipcMain/file.ts
+++ agent-launcher/electron/ipcMain/file.ts
@@ -1,25 +1,45 @@
-import { ipcMain } from "electron";
+import { app, ipcMain } from "electron";
 import { promises as fs } from "fs";
-import { EMIT_EVENT_NAME } from "../share/event-name.ts";
 import * as path from "path";
+import { EMIT_EVENT_NAME } from "../share/event-name.ts";
+
+/**
+ * ├─┬ agent-data
+ * │ ├─┬ agents
+ * │ │ ├── agent_1.js
+ * │ │ └── agent_2.js
+ * │
+ */
+const appDir = path.join(app.getPath('userData'), 'agent-data', 'agents');
+
+const checkAndCreateFolder = async (folderPath: string) => {
+  try {
+    await fs.access(folderPath);
+  } catch (error) {
+    await fs.mkdir(folderPath, { recursive: true });
+  }
+}
 
 const ipcMainSafeFile = () => {
    ipcMain.handle(EMIT_EVENT_NAME.READ_FILE, async (_, fileName) => {
-      const filePath = path.join(process.cwd(), fileName);
+      await checkAndCreateFolder(appDir);
+      const filePath = path.join(appDir, fileName);
       return fs.readFile(filePath, 'utf-8');
    });
    ipcMain.handle(EMIT_EVENT_NAME.WRITE_FILE, async (_, fileName, content) => {
-      const filePath = path.join(process.cwd(), fileName);
+      await checkAndCreateFolder(appDir);
+      const filePath = path.join(appDir, fileName);
       await fs.writeFile(filePath, content, "utf8");
       return filePath;
    });
    ipcMain.handle(EMIT_EVENT_NAME.ACCESS_FILE, async (_, fileName) => {
       try {
-         const filePath = path.join(process.cwd(), fileName);
+         await checkAndCreateFolder(appDir);
+         const filePath = path.join(appDir, fileName);
          await fs.access(filePath);
          return true;
       } catch (error) {
-         return false
+         return false;
       }
    });
 };
