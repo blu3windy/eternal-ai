9071ac88e69dd344c6fa205509627b737d41b143
0x11235 <193847696+0x11235@users.noreply.github.com>
2025-02-27 11:12:28 +0700

    -: update UI

--- agent-launcher/src/components/layout/index.tsx
+++ agent-launcher/src/components/layout/index.tsx
@@ -3,15 +3,19 @@ import { PropsWithChildren } from "react";
 import Header from "../header";
 import s from "./styles.module.scss";
 import ToastOverlay from "@components/ToastOverlay";
+import cs from "clsx";
 
-type Props = {};
+type Props = {
+  className?: string;
+  bodyClassName?: string;
+};
 
 const MainLayout = (props: Props & PropsWithChildren) => {
-   const { children } = props;
+   const { children, className, bodyClassName } = props;
    return (
-      <Flex className={s.container}>
+      <Flex className={cs(s.container, className)}>
          <Header />
-         <Box className={s.content}>{children}</Box>
+         <Box className={cs(s.content, bodyClassName)}>{children}</Box>
         <ToastOverlay />
       </Flex>
    );
