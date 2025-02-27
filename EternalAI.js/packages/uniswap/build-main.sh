3395d540bbc780c2e28835b84ee99375f3c4d9fc
0xgen0000 <52655944+0xgen0000@users.noreply.github.com>
2025-02-27 13:44:35 +0700

    - update

--- EternalAI.js/packages/uniswap/build-main.sh
+++ EternalAI.js/packages/uniswap/build-main.sh
@@ -3,3 +3,8 @@
 rm -rf ./dist-main
 
 yarn build-main
+gzip -c dist/bundle.es.js > dist/bundle.es.js.gz
+
+base64 -i ./dist/bundle.es.js.gz -o ./dist/bundle.iife.es.gz.base64
+sed -i '' 's|^|data:@file/gzip;base64,|' ./dist/bundle.es.js.gz.base64
+ls -lg -h dist-main/
