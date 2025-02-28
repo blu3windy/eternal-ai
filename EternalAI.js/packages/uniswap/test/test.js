96fca65eac3c29f926faef9431c0d95d44c0b564
0xgen0000 <52655944+0xgen0000@users.noreply.github.com>
2025-02-28 11:55:36 +0700

    - update

--- EternalAI.js/packages/uniswap/test/test.js
+++ EternalAI.js/packages/uniswap/test/test.js
@@ -8,6 +8,7 @@ const require = createRequire(import.meta.url); // Để sử dụng CommonJS
 function atob(str) {
     return Buffer.from(str, 'base64').toString('binary');
 }
+
 async function loadLibs(lib) {
     const response = await fetch(lib);
     const ethersScript = await response.text();
@@ -16,12 +17,9 @@ async function loadLibs(lib) {
     const context = {global: {}, atob, require};
     script.runInNewContext(context);
 
-    return context.global.ethers;
+    // return context.global.ethers;
 }
 
-async function main() {
-    await loadLibs("https://cdn.jsdelivr.net/npm/ethers@5.7.2/dist/ethers.umd.min.js")
-    await prompt("How much for 1 PEPE in USDT", "0x4a3dc71d1baa9405136dd496bd4a60193f37ccc6d5e47c1799e728339a007d1c")
-}
-
-main().catch(console.error);
+await loadLibs("https://cdn.jsdelivr.net/npm/ethers@5.7.2/dist/ethers.umd.min.js")
+const content = await prompt("Swap 1 PEPE to USDT", "0x4a3dc71d1baa9405136dd496bd4a60193f37ccc6d5e47c1799e728339a007d1c")
+console.log(content);
