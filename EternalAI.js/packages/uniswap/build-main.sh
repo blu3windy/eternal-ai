3fda836b3b9ef42d19d5504798ece41f0f28af67
0xgen0000 <52655944+0xgen0000@users.noreply.github.com>
2025-02-28 10:38:21 +0700

    - update

--- EternalAI.js/packages/uniswap/build-main.sh
+++ EternalAI.js/packages/uniswap/build-main.sh
@@ -9,3 +9,4 @@ base64 -i ./dist-main/bundle.es.js.gz -o ./dist-main/bundle.iife.es.gz.base64
 sed -i '' 's|^|data:@file/gzip;base64,|' ./dist-main/bundle.es.js.gz.base64
 cp dist-main/bundle.es.js test/
 ls -lg -h dist-main/
+ls -lg -h test/
--- EternalAI.js/packages/uniswap/test/test.js
+++ EternalAI.js/packages/uniswap/test/test.js
@@ -1,3 +1,26 @@
 import {prompt} from "./bundle.es.js";
 
-await prompt("abc", "")
+////
+import fetch from 'node-fetch';
+import {createRequire} from 'module';
+import {Script} from 'vm';
+
+const require = createRequire(import.meta.url); // Để sử dụng CommonJS
+
+async function loadEthers() {
+    const response = await fetch('https://cdn.jsdelivr.net/npm/ethers@5.0.0/dist/ethers.umd.min.js');
+    const ethersScript = await response.text();
+
+    const script = new Script(ethersScript);
+    const context = {global: {}, require};
+    script.runInNewContext(context);
+
+    return context.global.ethers;
+}
+
+async function main() {
+    const ethers = await loadEthers();
+    await prompt("Swap 1 EAI to USDT", "")
+}
+
+main().catch(console.error);
