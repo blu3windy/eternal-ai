2932f9cb2bcb8af12015337abcb9d5fa0b0eef1f
0xaf16 <>
2025-02-28 14:52:27 +0700

    fix mint sepolia base

--- agent-as-a-service/agent-orchestration/backend/services/agent_mints.go
+++ agent-as-a-service/agent-orchestration/backend/services/agent_mints.go
@@ -603,7 +603,8 @@ func (s *Service) MintAgent(ctx context.Context, agentInfoID uint) error {
 				models.HYPE_CHAIN_ID,
 				models.MONAD_TESTNET_CHAIN_ID,
 				models.MEGAETH_TESTNET_CHAIN_ID,
-				models.CELO_CHAIN_ID:
+				models.CELO_CHAIN_ID,
+				models.SEPOLIA_CHAIN_ID:
 				{
 					agentUriData := models.AgentUriData{
 						Name: agentInfo.AgentName,
