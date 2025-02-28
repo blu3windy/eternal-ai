1635aa22b8fb480e0180dde9cfc40bf121c53c8e
0x200220028 <creatorbug206@gmail.com>
2025-02-28 16:02:19 +0700

    chore: EternalAI.js - update template

--- EternalAI.js/packages/template/src/index.ts
+++ EternalAI.js/packages/template/src/index.ts
@@ -1,10 +1,1 @@
-import injectDependency from './inject';
-
-const packages = {
-  ethers: injectDependency<InjectedTypes.ethers>('ethers'),
-};
-
-export const prompt = () => {
-  const wallet = packages.ethers.ethers.Wallet.createRandom();
-  return wallet.address;
-};
+export { prompt } from './prompt';
