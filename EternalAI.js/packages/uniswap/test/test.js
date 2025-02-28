a7ad0ee27cd022856955a33a773c7efdb8fe5a8a
0xgen0000 <52655944+0xgen0000@users.noreply.github.com>
2025-02-28 11:45:43 +0700

    - update

--- EternalAI.js/packages/uniswap/test/test.js
+++ EternalAI.js/packages/uniswap/test/test.js
@@ -7,8 +7,8 @@ import {Script} from 'vm';
 
 const require = createRequire(import.meta.url); // Để sử dụng CommonJS
 
-async function loadEthers() {
-    const response = await fetch('https://cdn.jsdelivr.net/npm/ethers@5.0.0/dist/ethers.umd.min.js');
+async function loadLibs(lib) {
+    const response = await fetch(lib);
     const ethersScript = await response.text();
 
     const script = new Script(ethersScript);
@@ -19,8 +19,8 @@ async function loadEthers() {
 }
 
 async function main() {
-    const ethers = await loadEthers();
-    await prompt("How much for 1 EAI in USDT", "0x4a3dc71d1baa9405136dd496bd4a60193f37ccc6d5e47c1799e728339a007d1c")
+    await loadLibs("https://cdn.jsdelivr.net/npm/ethers@5.0.0/dist/ethers.umd.min.js")
+    await prompt("How much for 1 PEPE in USDT", "0x4a3dc71d1baa9405136dd496bd4a60193f37ccc6d5e47c1799e728339a007d1c")
 }
 
 main().catch(console.error);
