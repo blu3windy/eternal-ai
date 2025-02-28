9046cfa4ed982a7543b7bac4ee2c14ead7a96927
0x200220028 <creatorbug206@gmail.com>
2025-02-28 16:12:07 +0700

    chore: EternalAI.js - update template

--- EternalAI.js/packages/template/rollup.config.js
+++ EternalAI.js/packages/template/rollup.config.js
@@ -9,12 +9,22 @@ const builtins = require('rollup-plugin-node-builtins');
 const globals = require('rollup-plugin-node-globals');
 const inject = require('@rollup/plugin-inject');
 
+const { uglify } = require('rollup-plugin-uglify');
+
 module.exports = {
   input: ['./src/index.ts'],
   output: [
     {
-      dir: 'dist',
+      // dir: 'dist',
       format: 'cjs',
+      file: 'dist/index.cjs.js',
+      sourcemap: false,
+    },
+    {
+      // dir: 'dist',
+      format: 'umd',
+      file: 'dist/index.umd.js',
+      name: 'template',
       sourcemap: false,
     },
   ],
@@ -24,9 +34,10 @@ module.exports = {
     commonjs(),
     globals(),
     builtins(),
-    typescript({ tsconfig: './build.tsconfig.json' }),
+    typescript({ tsconfig: './build.tsconfig.json', declaration: false }),
     terser(),
     json(),
+    uglify(),
   ],
   external: [...Object.keys(pkg.peerDependencies || {}), 'ethers'],
 };
