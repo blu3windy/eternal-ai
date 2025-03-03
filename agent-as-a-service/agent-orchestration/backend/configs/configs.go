55cbb8ceb7bf9426141dd74fb5faa30cea7a1fb8
091090-T <091090-T>
2025-03-03 17:41:01 +0700

    update

--- agent-as-a-service/agent-orchestration/backend/configs/configs.go
+++ agent-as-a-service/agent-orchestration/backend/configs/configs.go
@@ -247,6 +247,7 @@ type Config struct {
 	LuckyMoneyAdminAddressSol   string                       `json:"lucky_money_admin_address_sol"`
 	WebhookUrl                  string                       `json:"webhook_url"`
 	KnowledgeBaseConfig         KnowledgeBaseConfig          `json:"knowledge_base_config"`
+	GetResultInferUrl           string                       `json:"get_result_infer_url"`
 	AgentDeployer               struct {
 		Url    string `json:"url"`
 		ApiKey string `json:"api_key"`
