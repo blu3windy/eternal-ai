b8372defd277b8f5788b284e199ec74265f97320
0x11235 <193847696+0x11235@users.noreply.github.com>
2025-03-04 16:06:08 +0700

    -: update

--- agent-launcher/src/pages/mine/index.tsx
+++ agent-launcher/src/pages/mine/index.tsx
@@ -1,10 +1,19 @@
 import MainLayout from "../../components/layout";
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
+import {useNavigate} from "react-router-dom";
+import ROUTERS from "@constants/route-path.ts";
 
 const Mine = () => {
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(ROUTERS.HOME);
+  };
+
    return (
       <MainLayout>
          <Box>Mine</Box>
+        <Text onClick={handleBack}>Back</Text>
       </MainLayout>
    );
 };
