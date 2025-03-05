8e74a25ebd913ebce5ca9f7ee9f9a0abe3471f3d
0x0603 <villatoto063@gmail.com>
2025-03-05 11:26:13 +0700

    chore: update code

--- agent-launcher/electron/ipcMain/index.ts
+++ agent-launcher/electron/ipcMain/index.ts
@@ -4,22 +4,7 @@ import ipcMainSafeFile from "./file.ts";
 import ipcMainDocker from "./docker.ts";
 import ipcMainModel from "./model.ts";
 
-
-function updatePath() {
-   const additionalPaths = ['/usr/local/bin', '/opt/homebrew/bin'];
-   const currentPath = process.env.PATH || '';
-
-   additionalPaths.forEach((path) => {
-      if (!currentPath.includes(path)) {
-         process.env.PATH += `:${path}`;
-      }
-   });
-
-   console.log(`Updated PATH: ${process.env.PATH}`);
-}
-
 const runIpcMain = () => {
-   updatePath();
    ipcMainDocker();
    ipcMainKeyChain();
    ipcMainSafeCopy();
