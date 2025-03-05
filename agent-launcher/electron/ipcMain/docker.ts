4d2519cf09e8cd2afcd74a2fe6d8d4d85f8136fa
0x0603 <villatoto063@gmail.com>
2025-03-05 16:11:42 +0700

    chore: update code

--- agent-launcher/electron/ipcMain/docker.ts
+++ agent-launcher/electron/ipcMain/docker.ts
@@ -16,6 +16,7 @@ const getDocker = async () => {
 }
 
 const DOCKER_NAME = 'launcher-agent';
+const DOCKER_SERVER_JS = `${DOCKER_NAME}-js`
 const DOCKER_ROUTER_NAME = `${DOCKER_NAME}-router`;
 
 const dockerCopyBuild = async () => {
@@ -85,7 +86,7 @@ const ipcMainDocker = () => {
          });
 
          await command.execAsyncDockerDir(
-            `cd "${folderPath}" && ${docker} build -t ${DOCKER_NAME} ./${USER_DATA_FOLDER_NAME.AGENT_JS}`
+            `cd "${folderPath}" && ${docker} build -t ${DOCKER_SERVER_JS} ./${USER_DATA_FOLDER_NAME.AGENT_JS}`
          );
 
          await command.execAsyncDockerDir(
@@ -187,8 +188,6 @@ const ipcMainDocker = () => {
       try {
          const userDataPath = app.getPath("userData");
          const folderPath = path.join(userDataPath, USER_DATA_FOLDER_NAME.AGENT_DATA);
-         // docker run -d -v /Users/nqhieu84/Work/testjs/abc/agents/app/1/prompt.js:/app/src/prompt.js --network network-agent-internal --name 1-agent1 agent
-         // docker run -d -v /Users/nqhieu84/Work/testjs/abc/agents/app/2/prompt.js:/app/src/prompt.js --network network-agent-external --network network-agent-internal --name 1-agent2 agent
          const dnsHost = `${chainId}-${agentName}`;
          const docker = await getDocker();
 
@@ -206,7 +205,7 @@ const ipcMainDocker = () => {
             )
          }
          const { stdout } = await command.execAsyncDockerDir(
-            `cd "${folderPath}" && ${docker} run -d -v ./agents/${dnsHost}/prompt.js:/app/src/prompt.js --network network-agent-internal --name ${dnsHost} ${DOCKER_NAME}`
+            `cd "${folderPath}" && ${docker} run -d -v ./agents/${dnsHost}/prompt.js:/app/src/prompt.js --network network-agent-internal --name ${dnsHost} ${DOCKER_SERVER_JS}`
          );
          console.log(stdout);
       } catch (error) {
