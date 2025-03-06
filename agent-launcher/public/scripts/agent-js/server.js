9f3d5317420de669e5064aafb40b377dd22fffd3
0x0603 <villatoto063@gmail.com>
2025-03-06 09:39:49 +0700

    chore: update code

--- agent-launcher/public/scripts/agent-js/server.js
+++ agent-launcher/public/scripts/agent-js/server.js
@@ -6,6 +6,7 @@ const PORT = 80;
 // Middleware to parse JSON and URL-encoded data
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true })); // For form data
+
 app.post('/prompt', async (req, res) => {
    try {
       console.log(req.body)
@@ -17,8 +18,8 @@ app.post('/prompt', async (req, res) => {
          privateKey,
          chainId,
       }
-      const responseMessage = await script.prompt(params);
-      res.send(responseMessage);
+      const message = await script.prompt(params);
+      res.send(message);
    } catch (error) {
       res.status(500).send(error?.message);
    }
