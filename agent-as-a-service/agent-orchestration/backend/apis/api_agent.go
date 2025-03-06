e2890268825da4bb93ac2cf6736576a710f8057b
0x250386 <>
2025-03-06 10:50:30 +0700

    update

--- agent-as-a-service/agent-orchestration/backend/apis/api_agent.go
+++ agent-as-a-service/agent-orchestration/backend/apis/api_agent.go
@@ -438,7 +438,7 @@ func (s *Server) GetDashBoardAgentDetail(c *gin.Context) {
 		return
 	}
 	userAddress, err := s.getUserAddressFromTK1Token(c)
-	ms, _, err := s.nls.GetDashboardAgentInfos(ctx, userAddress, chain, 0, []int{}, tokenAddress, search, "",
+	ms, _, err := s.nls.GetDashboardAgentInfos(ctx, []string{}, userAddress, chain, 0, []int{}, tokenAddress, search, "",
 		nil, sortStr, page, limit)
 
 	if err != nil {
