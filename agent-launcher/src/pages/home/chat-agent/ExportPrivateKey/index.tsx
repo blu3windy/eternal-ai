2239b704cd741ce1e3261860bedc4a211386a38c
0x11235 <193847696+0x11235@users.noreply.github.com>
2025-03-07 06:09:43 +0700

    -: add export private key

--- agent-launcher/src/pages/home/chat-agent/ExportPrivateKey/index.tsx
+++ agent-launcher/src/pages/home/chat-agent/ExportPrivateKey/index.tsx
@@ -1,37 +1,22 @@
-import {Box, Button, Flex, Text} from "@chakra-ui/react";
+import {Box, Flex, Text} from "@chakra-ui/react";
 import s from './styles.module.scss';
 import React, {useContext} from "react";
 import {AgentContext} from "@pages/home/provider";
 import copy from "copy-to-clipboard";
 import toast from "react-hot-toast";
-import localStorageService from "../../../../storage/LocalStorageService.ts";
-import STORAGE_KEYS from "@constants/storage-key.ts";
-import uniq from "lodash.uniq";
 
-const BackupPrivateKey = () => {
-  const { selectedAgent, agentWallet, setIsBackupedPrvKey } = useContext(AgentContext);
+const ExportPrivateKey = () => {
+  const { agentWallet, } = useContext(AgentContext);
 
   const onClickCopy = (address: string) => {
     copy(address);
     toast.success('Copied.');
   };
-  
-  const handleContinue = () => {
-    const agentIds = JSON.parse(localStorageService.getItem(STORAGE_KEYS.AGENTS_HAS_BACKUP_PRV_KEY)!);
-
-    localStorageService.setItem(STORAGE_KEYS.AGENTS_HAS_BACKUP_PRV_KEY, JSON.stringify(agentIds ? uniq([...agentIds, selectedAgent?.id]) : [selectedAgent?.id]));
-
-    setIsBackupedPrvKey(true);
-  }
 
   return (
-    <Flex className={s.container} h={"100%"} direction={"column"} justifyContent={"center"} alignItems={"center"} gap={"32px"}>
-      <Flex direction={"column"} justifyContent={"center"} gap={"16px"}>
-        <Text fontSize={"28px"} fontWeight={500} textAlign={"center"}>Back up private key</Text>
-        <Text fontSize={"18px"} fontWeight={400} textAlign={"center"} opacity={0.7}>Your private key makes it easy to back up<br/> and restore your account.</Text>
-      </Flex>
-      <Flex direction={"column"} gap={"12px"}>
-        <Flex direction={"column"} gap={"20px"} w={'590px'} borderRadius={"28px"} border={"1px dashed #E1DDDD"} p={"50px 110px"}>
+    <Flex className={s.container} h={"100%"} direction={"column"} justifyContent={"center"} alignItems={"center"} gap={"32px"} py={"24px"}>
+      <Flex direction={"column"} gap={"12px"} w={"100%"}>
+        <Flex direction={"column"} gap={"20px"} w={'100%'} borderRadius={"28px"} border={"1px dashed #E1DDDD"} p={"50px 50px"}>
           <Text fontSize={"16px"} fontWeight={400} textAlign={"center"} opacity={0.6}>{agentWallet?.privateKey}</Text>
           <Flex className={s.copyBox} alignItems={"center"} gap={"0px"} onClick={() => onClickCopy(agentWallet?.privateKey || '')}>
             <Text color="#000" fontSize="14px" fontWeight={400}>
@@ -44,7 +29,7 @@ const BackupPrivateKey = () => {
             </Box>
           </Flex>
         </Flex>
-        <Flex alignItems={"center"} gap={"8px"} justifyContent={"center"}>
+        <Flex alignItems={"center"} gap={"8px"}>
           <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g opacity="0.4">
               <path d="M10 1.25C12.3206 1.25 14.5462 2.17187 16.1872 3.81282C17.8281 5.45376 18.75 7.67936 18.75 10C18.75 12.3206 17.8281 14.5462 16.1872 16.1872C14.5462 17.8281 12.3206 18.75 10 18.75C7.67936 18.75 5.45376 17.8281 3.81282 16.1872C2.17187 14.5462 1.25 12.3206 1.25 10C1.25 7.67936 2.17187 5.45376 3.81282 3.81282C5.45376 2.17187 7.67936 1.25 10 1.25ZM10 5C9.8413 4.99986 9.68431 5.03282 9.53907 5.09678C9.39382 5.16073 9.26352 5.25428 9.15647 5.37145C9.04942 5.48861 8.96799 5.62681 8.91737 5.77723C8.86676 5.92764 8.84807 6.08696 8.8625 6.245L9.31875 11.2525C9.33644 11.4209 9.41584 11.5767 9.54164 11.69C9.66743 11.8032 9.83072 11.8659 10 11.8659C10.1693 11.8659 10.3326 11.8032 10.4584 11.69C10.5842 11.5767 10.6636 11.4209 10.6812 11.2525L11.1363 6.245C11.1507 6.08706 11.132 5.92785 11.0815 5.77752C11.0309 5.62719 10.9496 5.48905 10.8427 5.37191C10.7358 5.25476 10.6056 5.16119 10.4605 5.09716C10.3154 5.03312 10.1586 5.00003 10 5ZM10 15C10.2652 15 10.5196 14.8946 10.7071 14.7071C10.8946 14.5196 11 14.2652 11 14C11 13.7348 10.8946 13.4804 10.7071 13.2929C10.5196 13.1054 10.2652 13 10 13C9.73478 13 9.48043 13.1054 9.29289 13.2929C9.10536 13.4804 9 13.7348 9 14C9 14.2652 9.10536 14.5196 9.29289 14.7071C9.48043 14.8946 9.73478 15 10 15Z" fill="black"/>
@@ -53,15 +38,8 @@ const BackupPrivateKey = () => {
           <Text fontSize={"16px"} fontWeight={400} opacity={0.6}>Never disclose your backup phrase. Write it down and store in a secure location.</Text>
         </Flex>
       </Flex>
-
-      <Button
-        className={s.btnInstall}
-        onClick={handleContinue}
-      >
-        Continue
-      </Button>
     </Flex>
   )
 };
 
-export default BackupPrivateKey;
+export default ExportPrivateKey;
