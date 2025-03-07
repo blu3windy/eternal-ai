4e98a8c573393420415153f19144c98f46e8ed64
0x0603 <villatoto063@gmail.com>
2025-03-07 16:04:10 +0700

    chore: update code

--- agent-launcher/src/components/Loggers/Loggers.button.tsx
+++ agent-launcher/src/components/Loggers/Loggers.button.tsx
@@ -1,9 +1,14 @@
-import { Button, Flex } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import { useState } from "react";
 import { useLoggersStore } from "@components/Loggers/useLogs.ts";
 
 const NUMBER = 10;
-const LoggersButton = () => {
+
+interface IProps {
+   clearLogs: () => void;
+}
+
+const LoggersButton = (props: IProps) => {
 
    const [count, setCount] = useState(0);
 
@@ -18,19 +23,38 @@ const LoggersButton = () => {
 
    return (
       <Flex
+         flexDirection="row"
+         alignItems="center"
+         gap="4px"
          position="absolute"
          zIndex="3"
          bottom="10px"
          right="10px"
-         backgroundColor={showLogs ? "green" : "transparent"}
-         onClick={onClick}
-         cursor="pointer"
-         padding={showLogs ? "12px" : "8px"}
-         fontSize="12px"
-         color="white"
-         borderRadius="md"
       >
-         {showLogs ? "Hide" : ""}
+         {showLogs && (
+            <Flex
+               backgroundColor={"red.500" }
+               onClick={props.clearLogs}
+               cursor="pointer"
+               padding={"12px"}
+               fontSize="12px"
+               color="white"
+               borderRadius="md"
+            >
+               Clear
+            </Flex>
+         )}
+         <Flex
+            backgroundColor={showLogs ? "green" : "transparent"}
+            onClick={onClick}
+            cursor="pointer"
+            padding={showLogs ? "12px" : "8px"}
+            fontSize="12px"
+            color="white"
+            borderRadius="md"
+         >
+            {showLogs ? "Hide" : ""}
+         </Flex>
       </Flex>
    );
 }
--- agent-launcher/src/components/Loggers/index.tsx
+++ agent-launcher/src/components/Loggers/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import { Box, Text, useColorModeValue, Code } from "@chakra-ui/react";
-import { useLoggersStore } from "@components/Loggers/useLogs.ts"; // Zustand store
+import { useLoggersStore } from "@components/Loggers/useLogs.ts";
+import LoggersButton from "@components/Loggers/Loggers.button.tsx"; // Zustand store
 
 interface LogEntry {
    type: "output" | "error";
@@ -37,96 +38,99 @@ const Loggers = () => {
    }, [logs]);
 
    return (
-      <Box
-         display={showLogs ? "block" : "none"}
-         w="full"
-         h="calc(100dvh - 50px)"
-         overflowY="hidden"
-         bg={useColorModeValue("white", "gray.800")}
-         p={4}
-         borderRadius="lg"
-         borderWidth="1px"
-         shadow="lg"
-         position="absolute"
-         bottom="30px"
-         right="0"
-         zIndex="2"
-      >
-         {/*<Flex gap="12px" flexWrap="wrap" maxWidth="600px" justifyContent="center" alignItems="center">*/}
-         {/*   <Button*/}
-         {/*      onClick={() => {*/}
-         {/*         console.log('Test Run Docker 1-leon');*/}
-         {/*         window.electronAPI.dockerRunAgent('leon', '1');*/}
-         {/*      }}*/}
-         {/*   >*/}
-         {/*      Run Docker*/}
-         {/*   </Button>*/}
-         {/*   <Button*/}
-         {/*      onClick={() => {*/}
-         {/*         window.electronAPI.modelInstall("bafkreiecx5ojce2tceibd74e2koniii3iweavknfnjdfqs6ows2ikoow6m");*/}
-         {/*      }}*/}
-         {/*   >*/}
-         {/*      INSTALL MODEL*/}
-         {/*   </Button>*/}
-         {/*   <Button*/}
-         {/*      onClick={() => {*/}
-         {/*         window.electronAPI.modelRun("bafkreiecx5ojce2tceibd74e2koniii3iweavknfnjdfqs6ows2ikoow6m");*/}
-         {/*      }}*/}
-         {/*   >*/}
-         {/*      RUN MODEL*/}
-         {/*   </Button>*/}
-         {/*   <Button*/}
-         {/*      onClick={() => {*/}
-         {/*         window.electronAPI.modelCheckInstall(["bafkreiecx5ojce2tceibd74e2koniii3iweavknfnjdfqs6ows2ikoow6m"]);*/}
-         {/*      }}*/}
-         {/*   >*/}
-         {/*      MODEL CHECK INSTALL*/}
-         {/*   </Button>*/}
-         {/*   <Button*/}
-         {/*      onClick={() => {*/}
-         {/*         window.electronAPI.modelCheckRunning().then((hash?: string) => {*/}
-         {/*            alert(hash ? `Model is running with hash: ${hash}` : "Model is not running");*/}
-         {/*         });*/}
-         {/*      }}*/}
-         {/*   >*/}
-         {/*      MODEL CHECK RUNNING*/}
-         {/*   </Button>*/}
-         {/*</Flex>*/}
+      <>
          <Box
-            ref={logRef}
-            overflowY="auto"
-            width="100%"
-            height="100%"
+            display={showLogs ? "block" : "none"}
+            w="full"
+            h="calc(100dvh - 50px)"
+            overflowY="hidden"
+            bg={useColorModeValue("white", "gray.800")}
+            p={4}
+            borderRadius="lg"
+            borderWidth="1px"
+            shadow="lg"
+            position="absolute"
+            bottom="30px"
+            right="0"
+            zIndex="2"
          >
-            {Object.keys(logs).length === 0 ? (
-               <Text textAlign="center" color="gray.500">
-                   No logs yet. Click the button to run a command.
-               </Text>
-            ) : (
-               Object.entries(logs).map(([cmd, entries], index) => (
-                  <Box key={index} p={3} borderRadius="md" mb={4} bg="gray.700" color="white">
-                     <Text fontWeight="bold" fontSize="md" mb={2}>
-                          Command: <Code>{cmd}</Code>
-                     </Text>
-                     {entries.map((log, logIndex) => (
-                        <Box
-                           key={logIndex}
-                           p={2}
-                           borderRadius="md"
-                           mb={1}
-                           bg={log.type === "error" ? "red.400" : "gray.600"}
-                        >
-                           {/*<Text fontWeight="bold">{log.type.toUpperCase()}:</Text>*/}
-                           <Code whiteSpace="pre-wrap" fontSize="12px">
-                              {log.message.trim()}
-                           </Code>
-                        </Box>
-                     ))}
-                  </Box>
-               ))
-            )}
+            {/*<Flex gap="12px" flexWrap="wrap" maxWidth="600px" justifyContent="center" alignItems="center">*/}
+            {/*   <Button*/}
+            {/*      onClick={() => {*/}
+            {/*         console.log('Test Run Docker 1-leon');*/}
+            {/*         window.electronAPI.dockerRunAgent('leon', '1');*/}
+            {/*      }}*/}
+            {/*   >*/}
+            {/*      Run Docker*/}
+            {/*   </Button>*/}
+            {/*   <Button*/}
+            {/*      onClick={() => {*/}
+            {/*         window.electronAPI.modelInstall("bafkreiecx5ojce2tceibd74e2koniii3iweavknfnjdfqs6ows2ikoow6m");*/}
+            {/*      }}*/}
+            {/*   >*/}
+            {/*      INSTALL MODEL*/}
+            {/*   </Button>*/}
+            {/*   <Button*/}
+            {/*      onClick={() => {*/}
+            {/*         window.electronAPI.modelRun("bafkreiecx5ojce2tceibd74e2koniii3iweavknfnjdfqs6ows2ikoow6m");*/}
+            {/*      }}*/}
+            {/*   >*/}
+            {/*      RUN MODEL*/}
+            {/*   </Button>*/}
+            {/*   <Button*/}
+            {/*      onClick={() => {*/}
+            {/*         window.electronAPI.modelCheckInstall(["bafkreiecx5ojce2tceibd74e2koniii3iweavknfnjdfqs6ows2ikoow6m"]);*/}
+            {/*      }}*/}
+            {/*   >*/}
+            {/*      MODEL CHECK INSTALL*/}
+            {/*   </Button>*/}
+            {/*   <Button*/}
+            {/*      onClick={() => {*/}
+            {/*         window.electronAPI.modelCheckRunning().then((hash?: string) => {*/}
+            {/*            alert(hash ? `Model is running with hash: ${hash}` : "Model is not running");*/}
+            {/*         });*/}
+            {/*      }}*/}
+            {/*   >*/}
+            {/*      MODEL CHECK RUNNING*/}
+            {/*   </Button>*/}
+            {/*</Flex>*/}
+            <Box
+               ref={logRef}
+               overflowY="auto"
+               width="100%"
+               height="100%"
+            >
+               {Object.keys(logs).length === 0 ? (
+                  <Text textAlign="center" color="gray.500">
+                      No logs yet. Click the button to run a command.
+                  </Text>
+               ) : (
+                  Object.entries(logs).map(([cmd, entries], index) => (
+                     <Box key={index} p={3} borderRadius="md" mb={4} bg="gray.700" color="white">
+                        <Text fontWeight="bold" fontSize="md" mb={2}>
+                             Command: <Code>{cmd}</Code>
+                        </Text>
+                        {entries.map((log, logIndex) => (
+                           <Box
+                              key={logIndex}
+                              p={2}
+                              borderRadius="md"
+                              mb={1}
+                              bg={log.type === "error" ? "red.400" : "gray.600"}
+                           >
+                              {/*<Text fontWeight="bold">{log.type.toUpperCase()}:</Text>*/}
+                              <Code whiteSpace="pre-wrap" fontSize="12px">
+                                 {log.message.trim()}
+                              </Code>
+                           </Box>
+                        ))}
+                     </Box>
+                  ))
+               )}
+            </Box>
          </Box>
-      </Box>
+         <LoggersButton clearLogs={() => setLogs({})} />
+      </>
    );
 };
 
