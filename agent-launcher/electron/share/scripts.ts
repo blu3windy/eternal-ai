d89b8fa41ffa1e96404abe109629f9e032cab5c2
0x0603 <villatoto063@gmail.com>
2025-03-08 14:23:46 +0700

    chore: update code

--- agent-launcher/electron/share/scripts.ts
+++ agent-launcher/electron/share/scripts.ts
@@ -3,54 +3,58 @@ import path from "path";
 import fs from "fs";
 import { getScriptPath, PUBLIC_SCRIPT, SCRIPTS_NAME, USER_DATA_FOLDER_NAME } from "./utils.ts";
 
+const FILES_TO_COPY = {
+   [USER_DATA_FOLDER_NAME.AGENT_JS]: [
+      SCRIPTS_NAME.DOCKER_FILE,
+      SCRIPTS_NAME.PACKAGE_JSON,
+      SCRIPTS_NAME.SERVER_JS
+   ],
+   [USER_DATA_FOLDER_NAME.AGENT_ROUTER]: [
+      SCRIPTS_NAME.DOCKER_FILE,
+      SCRIPTS_NAME.PACKAGE_JSON,
+      SCRIPTS_NAME.SERVER_JS
+   ],
+   [USER_DATA_FOLDER_NAME.MODEL]: [SCRIPTS_NAME.MODEL_STARTER],
+   [USER_DATA_FOLDER_NAME.DOCKER]: [SCRIPTS_NAME.DOCKER_INSTALL_SCRIPT]
+}
+
 const copyFiles = async () => {
    const userDataPath = app.getPath("userData");
+   console.log(userDataPath);
 
-   console.log(userDataPath)
+   const convertToUpperCase = (str: string) => str.toUpperCase().replace(/-/g, "_");
 
-   const filesToCopy = {
-      [USER_DATA_FOLDER_NAME.AGENT_JS]: [
-         SCRIPTS_NAME.DOCKER_FILE,
-         SCRIPTS_NAME.PACKAGE_JSON,
-         SCRIPTS_NAME.SERVER_JS
-      ],
-      [USER_DATA_FOLDER_NAME.AGENT_ROUTER]: [
-         SCRIPTS_NAME.DOCKER_FILE,
-         SCRIPTS_NAME.PACKAGE_JSON,
-         SCRIPTS_NAME.SERVER_JS
-      ],
-      [USER_DATA_FOLDER_NAME.MODEL]: [SCRIPTS_NAME.MODEL_STARTER],
-      [USER_DATA_FOLDER_NAME.DOCKER]: [SCRIPTS_NAME.DOCKER_INSTALL_SCRIPT]
+   const createFolderPaths = (basePath: string, folders: Record<string, string[]>) => {
+      return Object.fromEntries(
+         Object.keys(folders).map(key => [
+            key,
+            path.join(basePath, USER_DATA_FOLDER_NAME.AGENT_DATA, USER_DATA_FOLDER_NAME[convertToUpperCase(key)])
+         ])
+      );
    };
 
-
-   const convertToUpperCase = (str: string) => {
-      return str.toUpperCase().replace(/-/g, "_");
-   }
-
-   const folderPaths = Object.fromEntries(
-      Object.keys(filesToCopy).map(key => [
-         key,
-         path.join(userDataPath, USER_DATA_FOLDER_NAME.AGENT_DATA, USER_DATA_FOLDER_NAME[convertToUpperCase(key)])
-      ])
-   );
-
-
-   for (const folderPath of Object.values(folderPaths)) {
-      if (!fs.existsSync(folderPath)) {
-         fs.mkdirSync(folderPath, { recursive: true });
+   const ensureFoldersExist = (folders: Record<string, string>) => {
+      for (const folderPath of Object.values(folders)) {
+         if (!fs.existsSync(folderPath)) {
+            fs.mkdirSync(folderPath, { recursive: true });
+         }
       }
-   }
+   };
 
-   for (const [key, files] of Object.entries(filesToCopy)) {
-      for (const file of files) {
-         console.log("dockerCopyBuild", Object.values(folderPaths));
-         const source = getScriptPath(file, `${PUBLIC_SCRIPT}/${USER_DATA_FOLDER_NAME[convertToUpperCase(key)]}`);
-         const destination = path.join(folderPaths[key], file);
-         fs.copyFileSync(source, destination);
+   const copyFilesToFolders = (folders: Record<string, string>, files: Record<string, string[]>) => {
+      for (const [key, fileList] of Object.entries(files)) {
+         for (const file of fileList) {
+            const source = getScriptPath(file, `${PUBLIC_SCRIPT}/${USER_DATA_FOLDER_NAME[convertToUpperCase(key)]}`);
+            const destination = path.join(folders[key], file);
+            fs.copyFileSync(source, destination);
+         }
       }
-   }
-}
+   };
+
+   const folderPaths = createFolderPaths(userDataPath, FILES_TO_COPY);
+   ensureFoldersExist(folderPaths);
+   copyFilesToFolders(folderPaths, FILES_TO_COPY);
+};
 
 export {
    copyFiles
