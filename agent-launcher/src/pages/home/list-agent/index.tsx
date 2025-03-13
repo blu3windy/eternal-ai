8e2d44ece4dc7e84b99756e8e7b873babb1383b6
0x11235 <193847696+0x11235@users.noreply.github.com>
2025-03-13 17:27:11 +0700

    -: add category all

--- agent-launcher/src/pages/home/list-agent/index.tsx
+++ agent-launcher/src/pages/home/list-agent/index.tsx
@@ -44,6 +44,7 @@ export const SortBy = [
 ];
 
 export enum CategoryOption {
+   All = 'all',
   Model = 'model',
   Utility = 'non-model',
   Infra = 'infra',
@@ -56,7 +57,7 @@ export enum FilterOption {
 }
 
 export const Category = [
-   // { value: FilterOption.All, label: 'All', description: 'All available agents.' },
+   { value: FilterOption.All, label: 'All', description: 'All available agents.' },
    { value: CategoryOption.Character, label: 'Character', description: 'Agents with unique personalities, offering engaging chat experiences and interactions.' },
    { value: CategoryOption.Model, label: 'Model', description: 'Agents providing direct access to specific AI models (LLaMA, DeepSeek, Hermes,…).' },
    { value: CategoryOption.Utility, label: 'Utility', description: 'Task-focused agents built with Python or JavaScript.' },
