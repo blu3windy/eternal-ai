34cf9748cfa4592d7382e6c7dc8408d060408e5c
0x11235 <193847696+0x11235@users.noreply.github.com>
2025-03-17 10:52:41 +0700

    -: update model mode

--- agent-launcher/src/constants/models.ts
+++ agent-launcher/src/constants/models.ts
@@ -2,14 +2,14 @@ export const LLM_MODELS = [
    {
       type: 'LLM',
       name: 'unsloth_Phi-4-mini-instruct-GGUF_Q8_0',
-      hash: 'bafkreih572a2uvcvyzpvdhvbgrplf6jdfgcvstb5ahvk7nxmlh7r2wfzve',
+      hash: 'bafkreibl7efovdbo5vb3u6pcga2e2s7bqnyf7lrgaliaeldlaqtvgjmd4q',
       size: 4.08,
       ram: 4.85
    },
    {
       type: 'LLM',
       name: 'unsloth_DeepSeek-R1-Distill-Qwen-1.5B-GGU_Q8_0',
-      hash: 'bafkreiecx5ojce2tceibd74e2koniii3iweavknfnjdfqs6ows2ikoow6m',
+      hash: 'bafkreihgmgbsxt2l6v7mpsy5bkdge6zjpyrlcqc25rlrlwekjsjumwwxc4',
       size: 1.89,
       ram: 2.11
    },
@@ -23,8 +23,15 @@ export const LLM_MODELS = [
    {
       type: 'LLM',
       name: 'Gemma-4B-it-Q8',
-      hash: 'bafkreibpmljh4udmm33ozih2dr6hm7e3pwhtlly3v5ys7i3mvgrasjfncu',
+      hash: 'bafkreiajytj4we555rx4idbhudklkg5zyrtd6qtb3vvsuqzridgy5s7h24',
       size: 4.12,
       ram: 4.94
+   },
+   {
+      type: 'LLM',
+      name: 'Gemma-1B-it-Q8',
+      hash: 'bafkreideybeu6dpfurmbhqhs2ylscys2gkimfgghg2fjg6ngrmo3wyk2uu',
+      size: 1,
+      ram: 1.23
    }
 ];
