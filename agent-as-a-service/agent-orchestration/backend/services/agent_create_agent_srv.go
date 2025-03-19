07ae7d7ecaf732b7410483914a64861f34c6f64f
770102 <770102>
2025-03-19 18:58:47 +0700

    update reply

--- agent-as-a-service/agent-orchestration/backend/services/agent_create_agent_srv.go
+++ agent-as-a-service/agent-orchestration/backend/services/agent_create_agent_srv.go
@@ -875,8 +875,8 @@ func (s *Service) AgentTwitterPostGenerateVideoByUserTweetId(ctx context.Context
 								for i := 0; i < 5; i++ {
 									time.Sleep(time.Duration(i*5) * time.Second)
 
-									contentReply := fmt.Sprintf("Here is your decentralized video.\n\nOnchain Prompt: https://basescan.org/tx/%v\nOnchain Video: https://basescan.org/tx/%v",
-										twitterPost.InferTxHash, twitterPost.SubmitSolutionTxHash)
+									contentReply := fmt.Sprintf("Hey @%v, here is your decentralized video.\n\nOnchain Prompt: https://basescan.org/tx/%v\nOnchain Video: https://basescan.org/tx/%v",
+										twitterPost.TwitterUsername, twitterPost.InferTxHash, twitterPost.SubmitSolutionTxHash)
 
 									//if twitterPost.TwitterID == "1576893241127219200" {
 									//	if twitterPost.TokenName != "" && twitterPost.TokenSymbol != "" {
