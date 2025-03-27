cc48cd0470907a3303b5190a06bfe9c70db80e81
0x11235 <193847696+0x11235@users.noreply.github.com>
2025-03-27 17:35:04 +0700

    fix build

--- agent-launcher/src/hooks/useDebounce.ts
+++ agent-launcher/src/hooks/useDebounce.ts
@@ -2,19 +2,19 @@ import { useEffect, useState } from 'react';
 import debounce from 'lodash/debounce';
 
 export function useDebounce<T>(value: T, delay: number): T {
-  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
-  useEffect(() => {
-    const handler = debounce(() => {
-      setDebouncedValue(value);
-    }, delay);
+   useEffect(() => {
+      const handler = debounce(() => {
+         setDebouncedValue(value);
+      }, delay);
 
-    handler();
+      handler();
 
-    return () => {
-      handler.cancel();
-    };
-  }, [value, delay]);
+      return () => {
+         handler.cancel();
+      };
+   }, [value, delay]);
 
-  return debouncedValue;
+   return debouncedValue;
 }
