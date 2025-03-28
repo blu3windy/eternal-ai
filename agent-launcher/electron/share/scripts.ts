e391baebd9f8f44dbac2a42ba1a2ba07bf5f40c4
0x666 <satan@devil.hell>
2025-03-28 09:37:43 +0700

    chore: agent-launcher - update script mount folder

--- agent-launcher/electron/share/scripts.ts
+++ agent-launcher/electron/share/scripts.ts
@@ -1,42 +1,43 @@
-import { app } from "electron";
-import path from "path";
-import fs from "fs";
-import { getScriptPath, PUBLIC_SCRIPT, SCRIPTS_NAME, USER_DATA_FOLDER_NAME } from "./utils.ts";
+import { app } from 'electron';
+import path from 'path';
+import fs from 'fs';
+import { getScriptPath, PUBLIC_SCRIPT, SCRIPTS_NAME, USER_DATA_FOLDER_NAME } from './utils.ts';
 
 const FILES_TO_COPY = {
    [USER_DATA_FOLDER_NAME.AGENT_JS]: [
       SCRIPTS_NAME.DOCKER_FILE,
       SCRIPTS_NAME.PACKAGE_JSON,
-      SCRIPTS_NAME.SERVER_JS
+      SCRIPTS_NAME.SERVER_JS,
    ],
    [USER_DATA_FOLDER_NAME.AGENT_ROUTER]: [
       SCRIPTS_NAME.DOCKER_FILE,
       SCRIPTS_NAME.PACKAGE_JSON,
-      SCRIPTS_NAME.SERVER_JS
+      SCRIPTS_NAME.SERVER_JS,
    ],
-   [USER_DATA_FOLDER_NAME.MODEL]: [
-      SCRIPTS_NAME.MODEL_STARTER,
-      SCRIPTS_NAME.MODEL_DOWNLOAD_BASE
-   ],
+   [USER_DATA_FOLDER_NAME.MODEL]: [SCRIPTS_NAME.MODEL_STARTER, SCRIPTS_NAME.MODEL_DOWNLOAD_BASE],
    [USER_DATA_FOLDER_NAME.DOCKER]: [
       SCRIPTS_NAME.DOCKER_INSTALL_SCRIPT,
       SCRIPTS_NAME.DOCKER_BUILD_SCRIPT,
       SCRIPTS_NAME.DOCKER_ACTION_SCRIPT,
       SCRIPTS_NAME.DOCKER_INFO_SCRIPT,
-   ]
-}
+   ],
+};
 
 const copyFiles = async () => {
-   const userDataPath = app.getPath("userData");
+   const userDataPath = app.getPath('userData');
    console.log(userDataPath);
 
-   const convertToUpperCase = (str: string) => str.toUpperCase().replace(/-/g, "_");
+   const convertToUpperCase = (str: string) => str.toUpperCase().replace(/-/g, '_');
 
    const createFolderPaths = (basePath: string, folders: Record<string, string[]>) => {
       return Object.fromEntries(
-         Object.keys(folders).map(key => [
+         Object.keys(folders).map((key) => [
             key,
-            path.join(basePath, USER_DATA_FOLDER_NAME.AGENT_DATA, USER_DATA_FOLDER_NAME[convertToUpperCase(key)])
+            path.join(
+               basePath,
+               USER_DATA_FOLDER_NAME.AGENT_DATA,
+               USER_DATA_FOLDER_NAME[convertToUpperCase(key)]
+            ),
          ])
       );
    };
@@ -49,10 +50,16 @@ const copyFiles = async () => {
       }
    };
 
-   const copyFilesToFolders = (folders: Record<string, string>, files: Record<string, string[]>) => {
+   const copyFilesToFolders = (
+      folders: Record<string, string>,
+      files: Record<string, string[]>
+   ) => {
       for (const [key, fileList] of Object.entries(files)) {
          for (const file of fileList) {
-            const source = getScriptPath(file, `${PUBLIC_SCRIPT}/${USER_DATA_FOLDER_NAME[convertToUpperCase(key)]}`);
+            const source = getScriptPath(
+               file,
+               `${PUBLIC_SCRIPT}/${USER_DATA_FOLDER_NAME[convertToUpperCase(key)]}`
+            );
             const destination = path.join(folders[key], file);
             fs.copyFileSync(source, destination);
          }
@@ -62,6 +69,10 @@ const copyFiles = async () => {
    const folderPaths = createFolderPaths(userDataPath, FILES_TO_COPY);
    ensureFoldersExist(folderPaths);
    copyFilesToFolders(folderPaths, FILES_TO_COPY);
+
+   ensureFoldersExist({
+      'agent-router-data-mount': `${folderPaths[USER_DATA_FOLDER_NAME.AGENT_ROUTER]}/data`,
+   });
 };
 
 const copyPublicToUserData = async (param: {
@@ -69,15 +80,14 @@ const copyPublicToUserData = async (param: {
    destination: string[];
    source: string[];
 }) => {
-   const userDataPath = app.getPath("userData");
+   const userDataPath = app.getPath('userData');
    for (const name of param.names) {
       const destinationFolder = path.join(userDataPath, ...param.destination);
       const destination = path.join(destinationFolder, name);
-      const source = getScriptPath(name,`${[PUBLIC_SCRIPT, ...param.source].join("/")}`);
+      const source = getScriptPath(name, `${[PUBLIC_SCRIPT, ...param.source].join('/')}`);
 
       // destination: '/Users/macbookpro/Library/Application Support/agent-launcher/agent-data/agent-js/package.json',
       //    source: '/Users/macbookpro/trustless-computer/eternal-ai/agent-launcher/public/scripts/agent-js/package.json'
-
 
       if (!fs.existsSync(destinationFolder)) {
          fs.mkdirSync(destinationFolder, { recursive: true });
@@ -85,9 +95,6 @@ const copyPublicToUserData = async (param: {
 
       fs.copyFileSync(source, destination);
    }
-}
+};
 
-export {
-   copyFiles,
-   copyPublicToUserData
-}
+export { copyFiles, copyPublicToUserData };
