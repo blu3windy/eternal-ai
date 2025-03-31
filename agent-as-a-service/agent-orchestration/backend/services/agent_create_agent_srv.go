5e53e7a3033970a68db9b2a9d672775b1a5d7b6b
0x1755 <>
2025-03-31 23:38:19 +0700

    update https

--- agent-as-a-service/agent-orchestration/backend/services/agent_create_agent_srv.go
+++ agent-as-a-service/agent-orchestration/backend/services/agent_create_agent_srv.go
@@ -902,7 +902,7 @@ func (s *Service) AgentTwitterPostGenerateVideoByUserTweetId(ctx context.Context
 
 									if s.conf.Clanker.IsCreateToken && twitterPost.TokenAddress != "" &&
 										twitterPost.TokenName != "" && twitterPost.TokenSymbol != "" {
-										contentReply = fmt.Sprintf("Hey @%v, here is your decentralized video.\n\nOnchain Video: basescan.org/tx/%v\n\nTicker $%s has been deployed.\n\n Contract address: %s\n\n Trade here: www.clanker.world/clanker/%s",
+										contentReply = fmt.Sprintf("Hey @%v, here is your decentralized video.\n\nOnchain Video: basescan.org/tx/%v\n\nTicker $%s has been deployed.\n\n Contract address: %s\n\n Trade here: https://www.clanker.world/clanker/%s",
 											twitterPost.TwitterUsername, twitterPost.SubmitSolutionTxHash, twitterPost.TokenSymbol, twitterPost.TokenAddress, twitterPost.TokenAddress)
 									}
 
