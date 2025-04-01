88052710a216b32ddf950c283d9045bc45e4bb60
0x11235 <193847696+0x11235@users.noreply.github.com>
2025-04-01 16:48:42 +0700

    update UI

--- agent-launcher/src/pages/home/chat-agent/AgentTopInfo/index.tsx
+++ agent-launcher/src/pages/home/chat-agent/AgentTopInfo/index.tsx
@@ -7,36 +7,32 @@ import {
    DrawerContent,
    DrawerOverlay,
    Flex,
+   Image,
    Text,
    useDisclosure,
-   Image,
 } from '@chakra-ui/react';
 import Avatar from '@components/Avatar';
-import BaseModal from '@components/BaseModal';
-import HeaderWallet from '@components/header/wallet';
 import InfoTooltip from '@components/InfoTooltip';
 import Percent24h from '@components/Percent';
 import { BASE_CHAIN_ID } from '@constants/chains';
 import CAgentContract from '@contract/agent';
 import SelectModel from '@pages/home/chat-agent/AgentTopInfo/SelectModel';
-import ExportPrivateKey from '@pages/home/chat-agent/ExportPrivateKey';
+import DeleteAgentModal from '@pages/home/list-agent/AgentMonitor/DeleteAgentModal';
 import { AgentType, SYSTEM_AGENTS } from '@pages/home/list-agent/constants';
 import { AgentContext } from '@pages/home/provider/AgentContext';
 import TradeAgent from '@pages/home/trade-agent';
 import AgentOnChainInfo from '@pages/home/trade-agent/onchain-info';
 import AgentTradeProvider from '@pages/home/trade-agent/provider';
 import CAgentTokenAPI from '@services/api/agents-token';
+import { IAgentToken } from '@services/api/agents-token/interface';
 import localStorageService from '@storage/LocalStorageService';
 import { formatCurrency } from '@utils/format.ts';
 import { addressFormater, compareString } from '@utils/string';
 import cx from 'clsx';
 import { useContext, useEffect, useMemo, useState } from 'react';
 import Jazzicon, { jsNumberForAddress } from 'react-jazzicon';
+import AgentWallet from '../../../../components/AgentWallet';
 import s from './styles.module.scss';
-import ProcessingTasks from './ProcessingTasks';
-import { IAgentToken } from '@services/api/agents-token/interface';
-import DeleteAgentModal from '@pages/home/list-agent/AgentMonitor/DeleteAgentModal';
-import { useDispatch } from 'react-redux';
 
 const AgentTopInfo = () => {
    const {
@@ -77,6 +73,10 @@ const AgentTopInfo = () => {
       if (hasNewVersionCode && isInstalled) {
          return 'red';
       }
+      return showSetup || (!isCanChat && !showBackupPrvKey) ? 'white' : 'black';
+   }, [isCanChat, showBackupPrvKey, showSetup, hasNewVersionCode, isInstalled]);
+   
+   const colorWallet = useMemo(() => {
       return showSetup || (!isCanChat && !showBackupPrvKey) ? 'white' : 'black';
    }, [isCanChat, showBackupPrvKey, showSetup, hasNewVersionCode, isInstalled]);
 
@@ -401,11 +401,7 @@ const AgentTopInfo = () => {
                      <Percent24h clsName={s.percent} percent={selectedAgent?.meme?.percent || 0} />
                   </Flex>
                   {selectedAgent?.required_wallet && !!agentWallet && isBackupedPrvKey && (
-                     <Button className={s.btnExport} onClick={handleExportPrvKey}>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
-                           <path d="M0 64C0 28.7 28.7 0 64 0L224 0l0 128c0 17.7 14.3 32 32 32l128 0 0 128-168 0c-13.3 0-24 10.7-24 24s10.7 24 24 24l168 0 0 112c0 35.3-28.7 64-64 64L64 512c-35.3 0-64-28.7-64-64L0 64zM384 336l0-48 110.1 0-39-39c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l80 80c9.4 9.4 9.4 24.6 0 33.9l-80 80c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l39-39L384 336zm0-208l-128 0L256 0 384 128z" />
-                        </svg>
-                     </Button>
+                     <AgentWallet color={colorWallet}/>
                   )}
                   <Button className={s.btnBuy} onClick={onOpenDrawer}>
                      Buy
@@ -416,9 +412,6 @@ const AgentTopInfo = () => {
                <HeaderWallet color={color} />
             </Flex> */}
          </Flex>
-         <BaseModal isShow={isOpen} onHide={onClose} title={'Export private key'} size="small">
-            <ExportPrivateKey />
-         </BaseModal>
          <Drawer isOpen={isOpenDrawer} placement="right" onClose={onCloseDrawer}>
             <DrawerOverlay />
             <DrawerContent bg={'#EDEDF2'} minW={'420px'}>
