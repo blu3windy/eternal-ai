1f2b32f1f6e4ec58a238af7d2ac02ad1f35c6a10
0x5123 <ngxx8651@gmail.com>
2025-04-02 13:55:02 +0700

    WIP

--- agent-launcher/electron/ipcMain/checkAndUpdateApps.ts
+++ agent-launcher/electron/ipcMain/checkAndUpdateApps.ts
@@ -10,9 +10,19 @@ const ipcCheckAndUpdateApp = () => {
 
   ipcMain.handle("quit-and-install", () => {
     console.log("quit-and-install");
-    // autoUpdater.quitAndInstall();
-    app.relaunch();
-    app.exit(0);
+    // autoUpdater.quitAndInstall(true, true);
+    // app.relaunch();
+    // app.exit(0);
+  });
+
+  ipcMain.handle("download-and-restart", () => {
+    console.log("download-and-restart");
+    autoUpdater.downloadUpdate().then(() => {
+      autoUpdater.quitAndInstall();
+    });
+    // autoUpdater.quitAndInstall(true, true);
+    // app.relaunch();
+    // app.exit(0);
   });
 };
 
