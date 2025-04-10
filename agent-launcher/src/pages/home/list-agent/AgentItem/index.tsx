940edd0094fded491b96ac615f36552f35922040
0x11235 <193847696+0x11235@users.noreply.github.com>
2025-04-10 10:21:01 +0700

    fix

--- agent-launcher/src/pages/home/list-agent/AgentItem/index.tsx
+++ agent-launcher/src/pages/home/list-agent/AgentItem/index.tsx
@@ -177,7 +177,7 @@ const AgentItem = ({ token, isLatest }: IProps) => {
                   </Text>
                </Flex>
                <Flex gap="4px" alignItems={'center'}>
-                  <Image src="`icons/ic-downloaded.svg`" w="15px" h="15px" />
+                  <Image src="icons/ic-downloaded.svg" w="15px" h="15px" />
                   <Text color="#657786" fontSize="12px" fontWeight="400">{formatCurrency(token.installed_count, 0, 0)}</Text>
                </Flex>
             </Flex>
