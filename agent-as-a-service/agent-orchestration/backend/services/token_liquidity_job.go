519d54680ceea6d591b7dba5c651eb3543be3a3d
0xaf16 <>
2025-04-14 14:41:30 +0700

    check token address

--- agent-as-a-service/agent-orchestration/backend/services/token_liquidity_job.go
+++ agent-as-a-service/agent-orchestration/backend/services/token_liquidity_job.go
@@ -398,6 +398,23 @@ func (s *Service) RetryAgentDeployToken(ctx context.Context, memeID uint) error 
 							if err != nil {
 								return errs.NewError(err)
 							}
+							// check token address exist in db
+							{
+								memeCheck, err := s.dao.FirstMeme(
+									daos.GetDBMainCtx(ctx),
+									map[string][]any{
+										"token_address = ?": {strings.ToLower(tokenAddress)},
+									},
+									map[string][]any{},
+									false,
+								)
+								if err != nil {
+									return errs.NewError(err)
+								}
+								if memeCheck != nil {
+									return errs.NewError(errs.ErrBadRequest)
+								}
+							}
 							err = daos.GetDBMainCtx(ctx).Model(&m).
 								Updates(
 									map[string]any{
