5b2f6db61e84a2ef8469580fbf95bce6e4ca97d7
0x0603 <villatoto063@gmail.com>
2025-04-14 17:02:52 +0700

    chore: update code

--- agent-launcher/src/providers/GarbageDocker/constants.ts
+++ agent-launcher/src/providers/GarbageDocker/constants.ts
@@ -1,8 +1,6 @@
 const WHITE_LIST_CONTAINER_NAMES = [
    'agent-router',
    '84532-proxy',
-   '84532-NewViteAgentYfinanceMcpV1'?.toLowerCase(),
-   '84532-QuickChart'?.toLowerCase(),
 ];
 
 export { WHITE_LIST_CONTAINER_NAMES };
